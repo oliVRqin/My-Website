cc80ef9db85b10edb16bc3a0c73edcf0e6c4f1d6
Oliver Qin <oliver.qin1@gmail.com>
2021-04-24 17:45:21 -0400

    removed calendar after merge with laptop

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -2,10 +2,7 @@ import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
 import Header from '../../components/header/index'
 import Link from 'next/link'
-<<<<<<< HEAD
 import { GA_TRACKING_ID } from '../../lib/gtag'
-=======
->>>>>>> 13a2f30fdbd6ab11f3fbf8600b7ba0f729e13f4d
 
 export default function About() {
   return (
